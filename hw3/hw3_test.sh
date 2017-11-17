7cbf3c7feb316bbd753e2ed47a1e4e9ed2feb7a5
KikiHungNTU <31952920+KikiHungNTU@users.noreply.github.com>
2017-11-17 22:43:51 +0800

    Add files via upload

---
+++ hw3/hw3_test.sh
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 17 22:36:02 2017
+
+@author: Ouch
+"""
+
+python3 hw3_test.py $1 $2
---
+++ hw3/hw3_train.sh
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 17 22:36:30 2017
+
+@author: Ouch
+"""
+
+python3 hw3_train.py $1
