861e4776177573704e54c3e8d228f207b80c955f
KikiHungNTU <31952920+KikiHungNTU@users.noreply.github.com>
2018-01-12 21:37:09 +0800

    Add files via upload

---
+++ hw6/hw6.sh
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 20:18:48 2018
+
+@author: Ouch
+"""
+
+python hw6.py $1 $2 $3
---
+++ hw6/pca.sh
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 20:18:48 2018
+
+@author: Ouch
+"""
+
+python PCA.py $1 $2
